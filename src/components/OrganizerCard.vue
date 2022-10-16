991386808c254aa9145b1bfdc7e3aa15b82b46f4
Nattaparin <nattaparin455@gmail.com>
2022-10-17 03:24:45 +0700

    3

---
+++ src/components/OrganizerCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <router-link
+    class="organizer-link"
+    :to="{ name: 'OrganizerLayoutView', params: { id: organizer.id } }"
+  >
+    <div class="organizer-card">
+      <h4>{{ organizer.name }}</h4>
+    </div>
+  </router-link>
+</template>
+<script>
+export default {
+  name: 'OrganizerCard',
+  props: {
+    organizer: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+.organizer-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #d5e44a;
+  margin-bottom: 18px;
+  margin: 20px 0 0 620px;
+}
+
+.organizer-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.organizer-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
